af3b43390a94fbfbe567494a62434369c42e3d83
Alexey 1981 <maximenko.av@rambler.ru>
2023-09-09 20:39:32 +0300

    Change main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double metrs=1.87;
-        double kilograms=98;
-        System.out.println((int) service.calculate(metrs,kilograms));
+        double metrs = 1.87;
+        double kilograms = 98;
+        System.out.println((int) service.calculate(metrs, kilograms));
 
     }
 }
